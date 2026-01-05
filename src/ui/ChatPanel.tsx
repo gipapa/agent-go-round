ebb9985f7c6626007b338aa9e45eaf56c735dca8
gipapa <gipapa.cs94@g2.nctu.edu.tw>
2026-01-05 17:25:41 +0800

    fix Chatpanel import

--- src/ui/ChatPanel.tsx
+++ src/ui/ChatPanel.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { ChatMessage } from "../types";
 
 export default function ChatPanel(props: { history: ChatMessage[]; onSend: (input: string) => Promise<void> }) {
